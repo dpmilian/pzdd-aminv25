f3ab1983c5fcf98edbbc1469b5fa66303bbb5887
Daniel <d.perez@scrobotics.es>
2025-11-16 19:06:40 +0100

    added nombres externally

---
+++ nombres.js
@@ -0,0 +1,19 @@
+const nombres = [
+    "Daniel",
+    "Maria Teresa",
+    "Sheila",
+    "Xoel",
+    "Jose",
+    "Luli",
+    "Thais",
+    "Manolito el Guapo",
+    "Isa",
+    "Maria Dolores",
+    "Lolita",
+    "Patri",
+    "Clara",
+    "Adri",
+    "David"
+];
+
+export default nombres;
